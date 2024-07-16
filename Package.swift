e54c798c226362180e0045fa8b23c61bc37af374
Michel Anderson Lutz Teixeira <michel_lutz@icloud.com>
2024-07-15 21:29:02 -0300

    Dependencia para DocC

--- Package.swift
+++ Package.swift
@@ -10,6 +10,9 @@ let package = Package(
             name: "WingedSwift",
             targets: ["WingedSwift"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/swiftlang/swift-docc-plugin", from: "1.1.0"),
+    ],
     targets: [
         .target(
             name: "WingedSwift"),
--- README.md
+++ README.md
@@ -135,7 +135,7 @@ This project is licensed under the MIT License. See the [LICENSE](LICENSE) file 
 
 
 ```bash
-swift package generate-documentation --target YourLibraryName --output-path ./docs
+swift package generate-documentation --target WingedSwift --output-path ./docs
 ```
 
 ```bash
