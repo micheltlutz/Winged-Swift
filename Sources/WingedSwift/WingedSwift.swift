dd1febe86a22332875f4888d80cd0cc63e07db2f
Michel Anderson Lutz Teixeira <michel_lutz@icloud.com>
2024-08-10 08:45:23 -0300

    enum  info now is public

--- Sources/WingedSwift/WingedSwift.swift
+++ Sources/WingedSwift/WingedSwift.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-enum WingedSwift: String {
+public enum WingedSwift: String {
     case VERSION = "1.1.0"
     case AUTHOR = "Michel Anderson Lutz Teixeira"
     case CONTACT = "https://micheltlutz.me"
